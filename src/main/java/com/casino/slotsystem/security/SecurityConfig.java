5ba5b6ca21fd2ce73856cd80e864902f5dfb741e
hnsu76579-coder <hnsu76579@gmail.com>
2026-02-11 15:31:59 +0530

    Add files via upload

--- src/main/java/com/casino/slotsystem/security/SecurityConfig.java
+++ src/main/java/com/casino/slotsystem/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -40,6 +41,10 @@ public class SecurityConfig {
 
                         .anyRequest().permitAll()
                 )
+                .sessionManagement(session ->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+
 
                 .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
