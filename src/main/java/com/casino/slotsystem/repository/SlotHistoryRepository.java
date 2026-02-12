fb39dd3272e2895a74ee669203bef1b82e8843bd
hnsu76579-coder <hnsu76579@gmail.com>
2026-02-12 21:03:53 +0530

    Update SlotHistoryRepository.java

--- src/main/java/com/casino/slotsystem/repository/SlotHistoryRepository.java
+++ src/main/java/com/casino/slotsystem/repository/SlotHistoryRepository.java
@@ -1,8 +1,7 @@
 package com.casino.slotsystem.repository;
 
 import com.casino.slotsystem.entity.SlotHistory;
-// import org.springframework.data.domain.Page;
-// import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -10,15 +9,11 @@ import java.util.List;
 
 public interface SlotHistoryRepository extends JpaRepository<SlotHistory, Long> {
 
-    // Page<SlotHistory> findBySlotIdOrderByChangedAtDesc(
-    //         Long slotId,
-    //         Pageable pageable
-    // );
-
     List<SlotHistory> findBySlotIdAndChangedAtAfterOrderByChangedAtDesc(
         Long slotId,
         LocalDateTime date
 );
 }
+
 
 
