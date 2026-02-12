b7aff8621da6c9b84a4a4eb1cef73af24088c8eb
hnsu76579-coder <hnsu76579@gmail.com>
2026-02-12 21:03:08 +0530

    Update SlotHistoryService.java

--- src/main/java/com/casino/slotsystem/service/SlotHistoryService.java
+++ src/main/java/com/casino/slotsystem/service/SlotHistoryService.java
@@ -3,8 +3,7 @@ package com.casino.slotsystem.service;
 import com.casino.slotsystem.dto.SlotHistoryResponse;
 import com.casino.slotsystem.repository.SlotHistoryRepository;
 import org.springframework.cache.annotation.Cacheable;
-// import org.springframework.data.domain.PageRequest;
-// import org.springframework.data.domain.Sort;
+
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,29 +19,7 @@ public class SlotHistoryService {
     public SlotHistoryService(SlotHistoryRepository repository) {
         this.repository = repository;
     }
-    // @Cacheable(
-    //         value = "slotHistory",
-    //         key = "#slotId + ':' + #page"
-    // )
-    // public List<SlotHistoryResponse> getSlotHistory(
-    //         Long slotId,
-    //         int page
-    // ) {
-    //     // 1 month ≈ 30 records
-    //     PageRequest pageable = PageRequest.of(
-    //             page,
-    //             30,
-    //             Sort.by("changedAt").descending()
-    //     );
-
-    //     return repository
-    //             .findBySlotIdOrderByChangedAtDesc(slotId, pageable)
-    //             .map(h -> new SlotHistoryResponse(
-    //                     h.getNumber(),
-    //                     h.getChangedAt()
-    //             ))
-    //             .getContent();
-    // }
+   
 
     @Cacheable(
         value = "slotHistory",
@@ -65,6 +42,7 @@ public List<SlotHistoryResponse> getSlotHistory(Long slotId) {
             .toList();
 }
 }
+
 
 
 
