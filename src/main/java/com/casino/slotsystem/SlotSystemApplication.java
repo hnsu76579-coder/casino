c57b30f011c734fa753612a732b3390c81b2d23d
hnsu76579-coder <hnsu76579@gmail.com>
2026-02-14 20:58:25 +0530

    Update SlotSystemApplication.java

--- src/main/java/com/casino/slotsystem/SlotSystemApplication.java
+++ src/main/java/com/casino/slotsystem/SlotSystemApplication.java
@@ -4,9 +4,10 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
-
+@EnableScheduling
 @EnableCaching
 @SpringBootApplication
 public class SlotSystemApplication {
